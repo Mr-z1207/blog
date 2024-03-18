18f4396353f816a0f4d23d8b8ec3860484e4b805
Mr1207 <wensongzhou@outlook.com>
2024-03-18 11:27:18 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,9 @@ git init
 git add -A
 git commit -m 'deploy'
 
+# 防止版本冲突
+git pull
+
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 git push -f git@github.com:Mr-z1207/blog.git master:gh-pages
 
